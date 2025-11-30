24893110a109a65c53849095df325197b222ce11
minthuhlaing16 <minthuhlaing786@gmail.com>
2025-11-30 21:38:24 +0630

    l52classmethod.py

--- parttwo/l52classmethod.py
+++ parttwo/l52classmethod.py
@@ -75,3 +75,74 @@ print(Book.getbooktype())
 print(Book.booktypevalid("PDF"))
 print(Book.booktypevalid("Paperback"))
 print(Book.booktypevalid("Ebook"))
+
+# todo ==> Alternative constructor
+
+
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    @classmethod
+    def calculateage(cls, name, birthyear):
+        currentyear = 2025
+        age = currentyear - birthyear
+        return cls(name, age)
+
+    @classmethod
+    def currentage(cls, name, birthyear):
+        from datetime import date
+
+        currentyear = date.today().year
+        age = currentyear - birthyear
+        return cls(name, age)
+
+
+# ? Normal instant
+personObj1 = Person("John Doe", 25)
+print(personObj1.name, personObj1.age)  # * John Doe 25
+
+# ? Using class method as alternative constructor
+personObj2 = Person.calculateage("Hla Hla", 1997)
+print(personObj2.name, personObj2.age)
+
+personObj3 = Person.currentage("Alice", 1996)
+print(personObj3.name, personObj3.age)
+
+
+# todo ==> Class Method with Inheritance
+
+
+class Student:
+    def __init__(self, name):
+        self.name = name
+
+    @classmethod
+    def describe(cls, level):
+        return f"A {cls.__name__} studies at {level}."
+
+
+class GraduateStudent(Student):
+    pass
+
+
+print(Student.describe("undergraduate"))
+print(GraduateStudent.describe("graduate"))
+
+
+# todo ==> Define Read-Only Property (@property)
+
+
+class Employee:
+    def __init__(self, name, monthlysalary):
+        self.name = name
+        self._monthlysalary = monthlysalary
+
+    @property
+    def annualsalary(self):
+        return self._monthlysalary * 12
+
+
+employeeObj = Employee("Zaw Zaw", 500)
+print(employeeObj.annualsalary)
