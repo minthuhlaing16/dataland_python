1eee0447ea7af22a85895ff89d4c6f7e03c39a00
minthuhlaing16 <minthuhlaing786@gmail.com>
2025-11-30 18:48:49 +0630

    Python l49 to l50 abstractmethod and staticmethod

---
+++ parttwo/l49abstractproperty.py
@@ -0,0 +1,114 @@
+from abc import ABC, abstractmethod
+
+
+class Person(ABC):
+    @property
+    @abstractmethod
+    def getrole(self):
+        pass
+
+
+# personObj = Person()  #! error
+
+
+class Engineer(Person):
+    @property
+    def getrole(self):
+        return "Professional Engineer."
+
+
+engineerObj = Engineer()
+print(engineerObj.getrole)  # * Professional Engineer.
+
+
+# todo ==> Exercise
+
+
+class Vehicle(ABC):
+    @property
+    @abstractmethod
+    def wheels(slef):
+        pass
+
+    @abstractmethod
+    def drive(self):
+        pass
+
+
+class Car(Vehicle):
+    @property
+    def wheels(self):
+        return 12
+
+    def drive(self):
+        return "Bus is driving on 12 wheels."
+
+
+carObj = Car()
+print(carObj.wheels)
+print(carObj.drive())
+
+
+# todo ==> Abstract Readonly Property
+
+
+class Employee(ABC):
+    @property
+    @abstractmethod
+    def id(self):
+        pass
+
+
+class Developer(Employee):
+    def __init__(self, empid):
+        self._empid = empid  # ? Protected attribute
+
+    @property
+    def id(self):
+        return self._empid
+
+
+developerObj = Developer(1001)
+print(developerObj.id)
+# developerObj.id = 1002  #! error ==> @property loa ma pyaw yin ya tal ==> AttributeError: property 'id' of 'Developer' object has no setter
+print(developerObj.id)
+
+
+# todo ==> Abstract Read/Write property (Getter/Setter)
+
+
+class Product(ABC):
+    @property
+    @abstractmethod
+    def price(self):
+        pass
+
+    @price.setter
+    @abstractmethod
+    def price(self, value):
+        pass
+
+
+class Book(Product):
+    def __init__(self, price):
+        self._price = price
+
+    @property
+    def price(self):
+        return self._price
+
+    @price.setter
+    def price(self, value):
+        if value < 0:
+            raise ValueError("Price cannot be negative value.")
+        self._price = value
+
+
+bookObj = Book(100)
+print(bookObj.price)
+
+bookObj.price = 50
+print(bookObj.price)
+
+# bookObj.price = -29
+# print(bookObj.price)
